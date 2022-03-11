11aca3688078fcadd53242c2fbabe91fb82cc0cc
marson parulian <marson.parulian@gmail.com>
2022-03-11 23:25:13 +1100

    handle invalid JSON : define specs

---
+++ tests/e2e/shows__invalid.test.ts
@@ -0,0 +1,10 @@
+import supertest from "supertest";
+import app from "../../src/app";
+import texts from "../../src/statics/texts";
+
+// This file contains test to handle invalid JSON
+
+describe("`show` in JSON : handle invalid JSON", () => {
+    test.todo("Handle `payload` is not an array");
+    test.todo("Handle `image    ` is undefined");
+});
