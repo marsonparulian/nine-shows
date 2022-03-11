adcf9573283b4a8463c3af03f78ffdec8700de44
marson parulian <marson.parulian@gmail.com>
2022-03-11 18:05:17 +1100

    init helper to convert 'IShow' to 'IShowCompact'

---
+++ src/helpers/show.helper.ts
@@ -0,0 +1,18 @@
+import { IShow, IShowCompact } from "../types/show";
+
+// This file contain helpers related to `show`
+
+/**
+ * Convert `IShow` typed object to `IShowCompact` type.
+ * @param show The origin object
+ * @returns converted object
+ */
+const toShowCompact = (show: IShow): IShowCompact => ({
+    image: show.image.showImage,
+    slug: show.slug,
+    title: show.title,
+});
+
+export default {
+    toShowCompact,
+}
