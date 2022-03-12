d9370ec3c9408819e3e07a4efd7d9e01e9aae8a5
marson parulian <marson.parulian@gmail.com>
2022-03-12 20:44:16 +1100

    fix : use process.env.PORT (glitch default)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import app from "./app";
 
 // Start app
-console.log("Starting server..");
-const port = 3000;
+console.log("Starting application server..");
+// Use defined environment variable (glitch.com) or set port to `80`
+const port: string | number = process.env.PORT || 80;
 app.listen(port, () => {
     console.log(`Server is started on port ${port}`);
 });
