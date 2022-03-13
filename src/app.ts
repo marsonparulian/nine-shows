8330467c9f010a076903d85748a77bce994cb4c6
marson parulian <marson.parulian@gmail.com>
2022-03-13 23:12:55 +1100

    init ErrorRequestHandler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import indexRouter from "./routers/index.router";
+import texts from "./statics/texts";
 
 // This file produces express' application object
 
 let app = express();
 app.use(express.json());
+
+// Handle `SyntaxError`
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    // Note : below is to be used after this code is working
+    // if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    if (err instanceof SyntaxError && "body" in err) {
+        return res.writeHead(400, {
+            "content-type": "application/json",
+        }).end(JSON.stringify({
+            message: texts.ERROR_INVALID_JSON,
+        }));
+    }
+    next();
+});
 
 // Set routers
 app.use("/", indexRouter);
